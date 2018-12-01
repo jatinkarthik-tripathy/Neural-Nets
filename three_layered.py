570b75572364f517fd05f861c418479dbf416f86
Jatin Karthik Tripathy <jatinkarthikt@gmail.com>
2018-12-01 12:10:22 +0530

    fixed the dimn, need to work on deltas

--- three_layered.py
+++ three_layered.py
@@ -8,8 +8,8 @@ def sigmoid(x, deriv=False):
 
 def main():
 	np.random.seed(0)
-	h = np.random.random((3,1))
-	w = np.random.random((1,1))
+	h = np.random.random((3, 1))
+	w = np.random.random((4, 1))
 	#print(h, w)
 
 	X = np.array([[150, 100, 100],
@@ -18,25 +18,25 @@ def main():
 				  [100, 100, 100]])
 	y = np.array([[1]])
 
-	for i in range(10000):
+	for i in range(1):
 		l0 = X
 		l0 = l0/255
 		l1 = sigmoid(np.dot(l0, h))
-		#print(l1)
-		l2 = sigmoid(np.dot(l1, w))
-		#print(l2)
+		#print(l1.T.shape)
+		l2 = sigmoid(np.dot(l1.T, w))
+		print(l2)
 
 		l2_error = y-l2
-		if (i % 1000) == 0:
-			print(l2_error)
-
+		#if (i % 1000) == 0:
+			#print(l2_error)
+		'''
 		l2_delta = l2_error*sigmoid(l2, deriv=True)
 		l1_error = l2_delta.dot(w.T)
 		l1_delta = l1_error*sigmoid(l1, deriv=True)
 
 		w = l1.T.dot(l2_delta)
 		h = l0.T.dot(l1_delta)
-
+		'''
 	print("-----------\n", l2)
 
 
